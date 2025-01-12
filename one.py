139a73fbeca73f438af3ce563bc8b73df81552b8
Lorcan <lorcanjames1@gmail.com>
2025-01-12 11:16:26 +0000

    first

---
+++ one.py
@@ -0,0 +1,14 @@
+import sys
+
+number = int(input())
+temp = []
+while number != 1:
+    temp.append(str(number) + " ")
+    # could check for a perfect square
+    if number & 1 == 0:
+        number //= 2
+    else:
+        number = (number * 3) + 1
+
+temp.append(str(number))
+print(''.join(temp))

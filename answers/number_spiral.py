c98321fb7e260d8c3593f629a59196725a9c7374
Lorcan <lorcanjames1@gmail.com>
2025-01-18 09:52:53 +0000

    remove redundant list

--- answers/number_spiral.py
+++ answers/number_spiral.py
@@ -3,7 +3,6 @@ cached_cols = {}
 cached_twos = {}
 
 n = int(input())
-answer = []
 
 for _ in range(n):
     row, col = map(int, input().split())
@@ -12,34 +11,28 @@ for _ in range(n):
     if row == col:
         if row not in cached_rows:
             cached_rows[row] = row ** 2
-        answer.append(cached_rows[row] - (row - 1))
+        print(cached_rows[row] - (row - 1))
     elif row > col:
         # means number is on the horizontal
         if row & 1 == 0:
             if row not in cached_twos:
                 cached_twos[row] = 2 ** row
-            ans = cached_twos[row]
-            answer.append(ans - (col - 1))
+            print(cached_twos[row] - (col - 1))
         else:
             if row not in cached_twos:
                 cached_twos[row] = 2 ** row
-            ans = cached_twos[row] + 1
-            answer.append(ans + (col - 1))
+            print(cached_twos[row] + 1 + (col - 1))
     elif col > row:
         # is odd
         if col & 1 == 1:
             if col not in cached_cols:
                 cached_cols[col] = col ** 2
-            ans = cached_cols[col]
-            answer.append(ans - (row - 1))
+            print(cached_cols[col] - (row - 1))
         else:
             if col - 1 not in cached_twos:
                 cached_twos[col - 1] = (col - 1) ** 2
-            ans = cached_twos[col - 1] + 1
-            answer.append(ans + (row - 1))
+            print(cached_twos[col - 1] + 1 + (row - 1))
 
-for t in answer:
-    print(t)
 
 
 
